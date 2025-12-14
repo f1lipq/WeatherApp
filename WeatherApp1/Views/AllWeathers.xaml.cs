d5cb882d9565b1c7f1f597eff820a33b52dd3501
f1lipq <fjamroz9@gmail.com>
2025-12-14 15:37:41 +0100

    updated nav tab from mainpage + fixed bugs

--- WeatherApp1/Views/AllWeathers.xaml.cs
+++ WeatherApp1/Views/AllWeathers.xaml.cs
@@ -1,11 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using static WeatherApp1.Views.MainPage;
+
 namespace WeatherApp1.Views;
 
 public partial class AllWeathers : ContentPage
 {
-	public AllWeathers()
+
+    public class AllWeathersData
+    {
+        public string Location { get; set; }
+        public string Icon { get; set; }
+        public int Temperature { get; set; }
+
+        public int HTemp { get; set; }
+
+        public int LTemp { get; set; }
+        
+        public string TempInfo { get; set; }
+    }
+
+    public ObservableCollection<AllWeathersData> WeathersAll { get; set; }
+
+    public AllWeathers()
 	{
 		InitializeComponent();
-	}
+
+        WeathersAll = new ObservableCollection<AllWeathersData>
+            {
+                new AllWeathersData { Location = "Krakow", Icon = "big_night_cloud_rain.png", Temperature = 19, HTemp = 21, LTemp = 15, TempInfo = "cloudy" },
+                new AllWeathersData { Location = "Tokyo", Icon = "big_tornado.png", Temperature = 18,HTemp = 21, LTemp = 15, TempInfo = "tornado"},
+                new AllWeathersData { Location = "Montreal", Icon = "big_day_cloud_rain1.png", Temperature = 17, HTemp = 24, LTemp = 15 },
+                new AllWeathersData { Location = "Barcelona", Icon = "big_tornado.png", Temperature = 16, HTemp = 18, LTemp = 15 },
+                new AllWeathersData { Location = "London", Icon = "big_night_cloud_rain.png", Temperature = 16, HTemp = 20, LTemp = 11 },
+                new AllWeathersData { Location = "Paris, France", Icon = "big_night_cloud_wind.png", Temperature = 17, HTemp = 26, LTemp = 17 },
+                new AllWeathersData { Location = "Warsaw, Poland", Icon = "big_day_cloud_rain1.png", Temperature = 19, HTemp = 21, LTemp = 14 },
+                new AllWeathersData { Location = "Berlin, Germany", Icon = "big_day_cloud_rain1.png", Temperature = 21, HTemp = 28, LTemp = 20 },
+                new AllWeathersData { Location = "Lizbona, Portugal", Icon = "big_day_cloud_rain2.png", Temperature = 23, HTemp = 25, LTemp = 22 },
+                new AllWeathersData { Location = "Rome, Italy", Icon = "big_tornado.png", Temperature = 25, HTemp = 27, LTemp = 18}
+            };
+
+        BindingContext = this;
+    }
 
     private async void GoToAllWeathers(object sender, EventArgs e)
     {
--- WeatherApp1/Views/MainPage.xaml.cs
+++ WeatherApp1/Views/MainPage.xaml.cs
@@ -24,16 +24,8 @@ namespace WeatherApp1.Views
             await Shell.Current.GoToAsync(nameof(MainPage));
         }
 
-        public class AllWeathersData
-        {
-            public string Location { get; set; }
-            public string Icon { get; set; }
-            public string Temperature { get; set; }
-        }
 
         public ObservableCollection<WeatherHourly> HourlyWeather { get; set; }
-        public ObservableCollection<AllWeathersData> WeathersAll { get; set; }
-
         public MainPage()
         {
             InitializeComponent();
@@ -52,21 +44,8 @@ namespace WeatherApp1.Views
                 new WeatherHourly { Time = "9 AM", Icon = "weathericonnight1.png", Temperature = "25℃" }
             };
 
-            WeathersAll = new ObservableCollection<AllWeathersData>
-            {
-                new AllWeathersData { Location = "Krakow, Poland", Icon = "weathericonnight1.png", Temperature = "19℃" },
-                new AllWeathersData { Location = "Tokyo, Japan", Icon = "weathericonnight1.png", Temperature = "18℃" },
-                new AllWeathersData { Location = "Montreal, Canada", Icon = "weathericonnight1.png", Temperature = "17℃" },
-                new AllWeathersData { Location = "Barcelona, Spain", Icon = "weathericonnight1.png", Temperature = "16℃" },
-                new AllWeathersData { Location = "London, England", Icon = "weathericonnight1.png", Temperature = "16℃" },
-                new AllWeathersData { Location = "Paris, France", Icon = "weathericonnight1.png", Temperature = "17℃" },
-                new AllWeathersData { Location = "Warsaw, Poland", Icon = "weathericonnight1.png", Temperature = "19℃" },
-                new AllWeathersData { Location = "Berlin, Germany", Icon = "weathericonnight1.png", Temperature = "21℃" },
-                new AllWeathersData { Location = "Lizbona, Portugal", Icon = "weathericonnight1.png", Temperature = "23℃" },
-                new AllWeathersData { Location = "Rome, Italy", Icon = "weathericonnight1.png", Temperature = "25℃" }
-            };
-
             BindingContext = this;
+
         }
     }
 }
